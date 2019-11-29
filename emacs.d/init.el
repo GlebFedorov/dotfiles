c212805e15fff00ffb557893caf6e60f19da952c
Gleb Fedorov <glebfedorov888@gmail.com>
2019-11-29 19:44:25 +0100

    Updated Configs

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -1,53 +1,209 @@
 (require 'package)
 (setq package-enable-at-startup nil)
-(add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
-(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
 (package-initialize)
 
-;; Install 'use-package' if necessary
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
-;; Enable use-package
-(eval-when-compile
-  (require 'use-package))
+(unless (package-installed-p 'atom-one-dark-theme)
+  (package-refresh-contents)
+  (package-install 'atom-one-dark-theme))
 
-;; Reasonable defaults
+(defvar my-term-shell "/bin/bash")
+(defadvice ansi-term (before force-bash)
+  (interactive (list my-term-shell)))
+(ad-activate 'ansi-term)
+
+(defalias 'yes-or-no-p 'y-or-n-p )
+
+(global-set-key (kbd "<M-return>") 'ansi-term)
+
+(use-package rainbow-delimiters
+  :ensure t
+  :init
+  (rainbow-delimiters-mode 1))
+
+(use-package which-key
+  :ensure t
+  :config
+  (which-key-mode))
+
+(use-package counsel
+  :ensure t)
+
+;; Swiper search
+(use-package swiper
+  :ensure t
+  :config
+  (progn
+    (ivy-mode 1)
+    (setq ivy-use-virtual-buffers t)
+    (setq enable-recursive-minibuffers t)
+    (global-set-key "\C-s" 'swiper)
+    (global-set-key (kbd "C-c C-r") 'ivy-resume)
+    (global-set-key (kbd "<f6>") 'ivy-resume)
+    (global-set-key (kbd "M-x") 'counsel-M-x)
+    (global-set-key (kbd "C-x C-f") 'counsel-find-file)
+    (global-set-key (kbd "<f1> f") 'counsel-describe-function)
+    (global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+    (global-set-key (kbd "<f1> l") 'counsel-find-library)
+    (global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+    (global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+    (global-set-key (kbd "C-c g") 'counsel-git)
+    (global-set-key (kbd "C-c j") 'counsel-git-grep)
+    (global-set-key (kbd "C-c k") 'counsel-ag)
+    (global-set-key (kbd "C-x l") 'counsel-locate)
+    (global-set-key (kbd "C-S-o") 'counsel-rhythmbox)
+    (define-key minibuffer-local-map (kbd "C-r") 'counsel-minibuffer-history)))
+
+;; Basic Dashboard setup
+(use-package dashboard
+  :ensure t
+  :config
+  (dashboard-setup-startup-hook)
+  (setq dashboard-items '((recents . 10)))
+  (setq dashboards-benner-logo-title "Something cool"))
+
+;; Basic convinience settings
 (setq scroll-conservatively 100)
 (setq ring-bell-function 'ignore)
 (when window-system (global-hl-line-mode t))
-(when window-system (global-prettify-symbols-mode t))
 (setq make-backup-file nil)
 (setq auto-save-default nil)
 (global-subword-mode 1)
 (global-display-line-numbers-mode 1)
+(global-auto-revert-mode 1)
+
+
+;;; CUSTOM FUNCTIONS ---------------------
+
+
+
+
+(defun my-kill-thing-at-point (thing)
+  "Kill the `thing-at-point' for the specified kind of THING."
+  (let ((bounds (bounds-of-thing-at-point thing)))
+    (if bounds
+        (kill-region (car bounds) (cdr bounds))
+      (error "No %s at point" thing))))
+
+(defun my-kill-word-at-point ()
+  "Kill the word at point."
+  (interactive)
+  (my-kill-thing-at-point 'word))
+
+(global-set-key (kbd "C-c w") 'my-kill-word-at-point)
+
+
+
+;; Popup kill ring
+(use-package popup-kill-ring
+  :ensure t
+  :bind ("M-y" . popup-kill-ring))
+
+
+
+(defun new-line-above()
+	  (interactive)
+	  (move-beginning-of-line nil)
+	  (newline)	
+	  (previous-line))
+
+(defun new-line-below()
+	  (interactive)
+	  (move-end-of-line nil)
+	  (newline))
+
+
+(global-set-key (kbd "C-c o") 'new-line-below)
+(global-set-key (kbd "C-c O") 'new-line-above)
+
+(defun duplicate-line (arg)
+  "Duplicate current line, leaving point in lower line."
+  (interactive "*p")
+
+  ;; save the point for undo
+  (setq buffer-undo-list (cons (point) buffer-undo-list))
+
+  ;; local variables for start and end of line
+  (let ((bol (save-excursion (beginning-of-line) (point)))
+        eol)
+    (save-excursion
+
+      ;; don't use forward-line for this, because you would have
+      ;; to check whether you are at the end of the buffer
+      (end-of-line)
+      (setq eol (point))
+
+      ;; store the line and disable the recording of undo information
+      (let ((line (buffer-substring bol eol))
+            (buffer-undo-list t)
+            (count arg))
+        ;; insert the line arg times
+        (while (> count 0)
+          (newline)         ;; because there is no newline in 'line'
+          (insert line)
+          (setq count (1- count)))
+        )
+
+      ;; create the undo information
+      (setq buffer-undo-list (cons (cons eol (point)) buffer-undo-list)))
+    ) ; end-of-let
+
+  ;; put the point in the lowest line and return
+  (next-line arg))
+
+(global-set-key (kbd "C-c d") 'duplicate-line )
+
+;;; ------------------------------------------
+
+
+
+
+
 
 ;; Decent appearence
 (scroll-bar-mode 0)
 (menu-bar-mode 0)
 (tool-bar-mode 0)
 
+;; Buffers
+(global-set-key (kbd "C-x b") 'ibuffer)
+(global-set-key (kbd "C-x C-b") 'ido-switch-buffer)
 
-;; Org mode
-(transient-mark-mode 1)
-(require 'org)
-(add-to-list 'auto-mode-alist '("\\.org$" . org-mode))
+;; Avy
+(use-package avy
+  :ensure t
+  :bind
+  ("M-s" . avy-goto-char)
+)
 
-;;Which Key
-(use-package which-key
+;; Open Config
+(defun config-visit ()
+  (interactive)
+  (find-file "~/.emacs.d/init.el"))
+(global-set-key (kbd "C-c e") 'config-visit)
+
+(use-package beacon
+  :ensure t
+  :init (beacon-mode 1))
+
+;; Switch-Window
+
+(use-package switch-window
   :ensure t
   :config
-  (which-key-mode 1))
-
-;; Magit
-(use-package magit
-  :ensure t
-  :config
-  (global-set-key (kbd "C-x M-g") 'magit-dispatch)
-  (global-set-key (kbd "C-x g") 'magit-status))
-
+  (setq switch-window-input-style 'minibuffer)
+  (setq switch-window-increase 4)
+  (setq switch-window-threshold 2)
+  (setq switch-window-shortcut-style 'qwerty)
+  (setq switch-window-qwerty-shortcuts
+	'("a" "s" "d" "f" "j" "k" "l"))
+  :bind
+  ([remap other-window] . switch-window))
 
 (defun split-and-follow-horizontally ()
   (interactive)
@@ -55,63 +211,45 @@
   (balance-windows)
   (other-window 1))
 (global-set-key (kbd "C-x 2") 'split-and-follow-horizontally)
-  
+
 (defun split-and-follow-vertically ()
   (interactive)
   (split-window-right)
   (balance-windows)
   (other-window 1))
-
 (global-set-key (kbd "C-x 3") 'split-and-follow-vertically)
 
-(defun go-to-config ()
-  (interactive)
-  (find-file "~/.emacs.d/init.el"))
-(global-set-key (kbd  "C-c e") 'go-to-config)
+;; Magit
+(use-package magit
+ :ensure t
+ :config
+ (global-set-key (kbd "C-x M-g") 'magit-dispatch)
+ (global-set-key (kbd "C-x g") 'magit-status))
 
-(defun copy-whole-line ()
-  (interactive)
-  (save-excursion
-    (kill-new
-     (buffer-substring
-      (point-at-bol)
-      (point-at-eol)))))
-(global-set-key (kbd "C-c C-l") 'copy-whole-line)
-
-(defun kill-inner-word ()
-  (interactive)
-  (backward-word)
-  (kill-word 1))
-(global-set-key (kbd "C-c w") 'kill-inner-word)
-
-
-;; Ivy
-(use-package ivy
-  :defer 0.1
-  :diminish
-  :bind (("C-c C-r" . ivy-resume)
-         ("C-x B" . ivy-switch-buffer-other-window))
-  :custom
-  (ivy-count-format "(%d/%d) ")
-  (ivy-use-virtual-buffers t)
-  :config (ivy-mode))
-
-(use-package counsel
-  :config (counsel-mode))
-
-(use-package ivy-rich
-  :after ivy
-  :custom
-  (ivy-virtual-abbreviate 'full
-                          ivy-rich-switch-buffer-align-virtual-buffer t
-                          ivy-rich-path-style 'abbrev)
+;; Space Line
+(use-package spaceline
+  :ensure t
   :config
-  (ivy-set-display-transformer 'ivy-switch-buffer
-                               'ivy-rich-switch-buffer-transformer))
+  (require 'spaceline-config)
+  (setq powerline-default-separator (quote arrow))
+  (spaceline-spacemacs-theme))
 
-(use-package swiper
-  :bind (("C-s" . swiper)
-         ("C-r" . swiper)))
+;; Autocompletion
+(use-package company
+  :ensure t
+  :init
+  (add-hook 'after-init-hook 'global-company-mode))
+
+;;Org-mode stuff
+(use-package org-bullets
+  :ensure t
+  :config
+  (add-hook 'org-mode-hook (lambda () (org-bullets-mode 1)))
+  (define-key global-map "\C-cl" 'org-store-link)
+  (define-key global-map "\C-ca" 'org-agenda)
+  (setq org-log-done t))
+
+(setq inhibit-startup-message t)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -120,13 +258,24 @@
  ;; If there is more than one, they won't work right.
  '(ansi-color-faces-vector
    [default default default italic underline success warning error])
- '(custom-enabled-themes (quote (atom-one-dark)))
+ '(ansi-color-names-vector
+   ["#21252B" "#E06C75" "#98C379" "#E5C07B" "#61AFEF" "#C678DD" "#56B6C2" "#ABB2BF"])
+ '(custom-enabled-themes nil)
  '(custom-safe-themes
    (quote
     ("669e02142a56f63861288cc585bee81643ded48a19e36bfdf02b66d745bcc626" default)))
+ '(fci-rule-color "#3E4451")
  '(package-selected-packages
    (quote
-    (evil which-key use-package switch-window swiper spaceline smex rainbow-delimiters popup-kill-ring magit ido-vertical-mode helm company beacon avy atom-one-dark-theme))))
+    (magit ivy-hydra counsel which-key use-package switch-window spaceline smex rainbow-delimiters popup-kill-ring org-bullets ido-vertical-mode dashboard company beacon avy atom-one-dark-theme)))
+ '(tetris-x-colors
+   [[229 192 123]
+    [97 175 239]
+    [209 154 102]
+    [224 108 117]
+    [152 195 121]
+    [198 120 221]
+    [86 182 194]]))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- nvim/init.vim
+++ nvim/init.vim
@@ -1,26 +1,28 @@
-set number
-syntax on
 call plug#begin('~/.vim/plugged')
-Plug 'https://github.com/rakr/vim-one'
+Plug 'Rigellute/rigel'
 Plug 'vim-airline/vim-airline'
-Plug 'tpope/vim-surround'
+Plug 'vim-airline/vim-airline-themes'
+Plug 'https://github.com/xavierd/clang_complete'
 Plug 'scrooloose/nerdtree'
-Plug 'PotatoesMaster/i3-vim-syntax'
+Plug 'tpope/vim-surround'
+Plug 'tpope/vim-fugitive'
 call plug#end()
 
-if (empty($TMUX))
-	  if (has("nvim"))
-		      let $NVIM_TUI_ENABLE_TRUE_COLOR=1
-		        endif
-			  if (has("termguicolors"))
-				      set termguicolors
-				        endif
-				endif
+let g:clang_library_path='/usr/lib/llvm-9/lib'
 
-set background=light        " for the light version
-let g:one_allow_italics = 1 " I love italic for comments
-colorscheme one
-let g:airline_theme='one'
+set termguicolors
+syntax enable
+colorscheme rigel 
+let g:rigel_airline = 1
+let g:airline_theme = 'rigel'
+set number
 set clipboard+=unnamed
 set clipboard=unnamedplus
 set clipboard=unnamed
+
+set tabstop=4
+set softtabstop=4
+set shiftwidth=4
+set noexpandtab
+set colorcolumn=110
+highlight ColorColumn ctermbg=darkgray
